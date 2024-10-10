f246a73f1f074582b5cb6a4163aecb1badb65486
ashvik01 <ashvikkhanna.2015@gmail.com>
2024-10-10 12:41:12 +0530

    Simple interest

---
+++ main.py
@@ -0,0 +1,8 @@
+p= float(input("Enter the principle: "))
+x= float(input("Enter the number of percentage: "))
+int= x/100
+t= float(input("Enter the number of years: "))
+
+a= (p*int*t)/100
+
+print(a)
